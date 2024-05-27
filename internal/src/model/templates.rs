ff1fc72f19b5dee8d1c9ad220fea0214b247cfb8
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-05-28 02:35:10 +0700

    feat(templates): add new model for blog(s)

--- internal/src/model/templates.rs
+++ internal/src/model/templates.rs
@@ -1,5 +1,18 @@
 use askama::Template;
 
-#[derive(Template)]
+#[derive(Template, Debug)]
 #[template(path = "profile.html")]
 pub struct Profile;
+
+#[derive(Template, Debug)]
+#[template(path = "blogs.html")]
+pub struct Blogs<'a> {
+    pub blogs: &'a Vec<Blog<'a>>,
+}
+
+#[derive(Template, Debug)]
+#[template(path = "blog.html")]
+pub struct Blog<'a> {
+    pub id: &'a str,
+    pub name: &'a str,
+}
