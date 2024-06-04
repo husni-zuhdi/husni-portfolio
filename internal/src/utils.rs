b92a0e99174550896967db8fb8ed351fe0bf0412
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-06-04 22:08:55 +0700

    feat(utils): add read version manifest helper function

--- internal/src/utils.rs
+++ internal/src/utils.rs
@@ -1,5 +1,7 @@
+use crate::model::data::VersionData;
 use markdown::to_html;
 use std::fs;
+use std::io::BufReader;
 
 /// md_to_html: Markdown to HTML
 /// take String of filename
@@ -7,4 +9,17 @@ use std::fs;
 pub fn md_to_html(filename: String) -> Result<String, String> {
     let body_md = fs::read_to_string(filename).expect("Failed to read markdown blog file");
     Ok(to_html(&body_md))
+}
+
+/// read_version_manifest
+/// read version manifest on root repository to get this configuration
+/// * version
+/// * git build hash
+/// * build date
+pub fn read_version_manifest() -> Result<VersionData, String> {
+    let file = fs::File::open("version.json").expect("Failed to open version.json");
+    let reader = BufReader::new(file);
+
+    let json: VersionData = serde_json::from_reader(reader).expect("Failed to parse version.json");
+    Ok(json)
 }
