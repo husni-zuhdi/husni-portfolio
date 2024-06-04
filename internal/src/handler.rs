fd7e7fad23aa4110eb6badf08f5c9ff00d23db0f
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-06-04 22:11:28 +0700

    feat(config): update config usage on handler

--- internal/src/handler.rs
+++ internal/src/handler.rs
@@ -2,8 +2,9 @@ use crate::config::Config;
 use crate::model::data::BlogsData;
 use crate::router::*;
 use actix_web::{middleware, web, App, HttpServer};
+use log::info;
 
-pub async fn handler(cfg: &Config) -> std::io::Result<()> {
+pub async fn handler(cfg: Config) -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or(cfg.log_level.clone()));
 
     let endpoint = cfg.svc_endpoint.as_str();
@@ -11,22 +12,25 @@ pub async fn handler(cfg: &Config) -> std::io::Result<()> {
         .svc_port
         .parse::<u16>()
         .expect("Failed to get port number");
+    // let version = cfg.version.clone();
+    let config = cfg.clone();
     let blogs_data = BlogsData::default();
 
-    log::info!(
+    info!(
         "Starting HTTP Server at http://{}:{}",
-        cfg.svc_endpoint,
-        cfg.svc_port
+        cfg.svc_endpoint, cfg.svc_port
     );
 
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
             .app_data(web::Data::new(blogs_data.clone()))
+            .app_data(web::Data::new(config.clone()))
             .service(web::resource("/").route(web::get().to(profile)))
             .service(web::resource("/styles.css").route(web::get().to(styles)))
-            .service(web::resource("/blogs").route(web::get().to(blogs)))
+            .service(web::resource("/blogs").route(web::get().to(get_blogs)))
             .service(web::resource("/blogs/{blogid}").route(web::get().to(get_blog)))
+            .service(web::resource("/version").route(web::get().to(get_version)))
     })
     .bind((endpoint, port))
     .expect("Failed to start Http Server")
