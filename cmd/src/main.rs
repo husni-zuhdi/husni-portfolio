15e9aeee14671071f90bd7df35b692ae03f186e5
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-06-04 22:12:56 +0700

    chore(cmd): change config usage from default => from_envar

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -2,6 +2,6 @@ use internal::{self, config::Config, handler::handler};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let config = Config::default();
-    handler(&config).await
+    let config = Config::from_envar();
+    handler(config).await
 }
