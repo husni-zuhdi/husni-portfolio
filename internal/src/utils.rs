317b9a1b054394494b862758b59950e1b9050d65
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-08-17 12:26:16 +0700

    feat: add gh link replacer helper function

--- internal/src/utils.rs
+++ internal/src/utils.rs
@@ -1,5 +1,7 @@
 use crate::model::data::VersionData;
+use log::debug;
 use markdown::to_html;
+use regex::Regex;
 use std::fs;
 use std::io::BufReader;
 
@@ -36,11 +38,56 @@ pub fn read_version_manifest() -> Result<VersionData, String> {
     Ok(json)
 }
 
-/// Capitalizes the first character in s.
+/// capitalize
+/// Capitalize the first character in s.
+/// Take borrowed str of s
+/// then return capitalized String
 pub fn capitalize(s: &str) -> String {
     let mut c = s.chars();
     match c.next() {
         None => String::new(),
         Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+/// replace_gh_link
+/// Replace Github Blog relative links
+/// with full github content links
+/// Take String of markdown body
+/// and String of github blog endpoint
+/// then return String of updated body
+pub fn replace_gh_link(body: String, gh_blog_link: String, gh_raw_blog_link: String) -> String {
+    let mut res = String::new();
+
+    // Regex href=.\.\/ mean
+    // find string with character 'href='
+    // then followed by any character (I tried to use '"' but didn't work)
+    // then followed by '.' (must use escape character)
+    // then followed by '/' (must use escape character)
+    let re_href = Regex::new(r"href=.\.\/").expect("Failed to build regex href");
+
+    let replaced_str_href = format!("href=\"{}/", gh_blog_link);
+    debug!("Replaced str: {}", &replaced_str_href);
+
+    res = re_href
+        .replace_all(body.as_str(), replaced_str_href.as_str())
+        .to_string();
+    debug!("Replaced Body: {}", &res);
+
+    // Regex src=.\.\/ mean
+    // find string with character 'src='
+    // then followed by any character (I tried to use '"' but didn't work)
+    // then followed by '.' (must use escape character)
+    // then followed by '/' (must use escape character)
+    let re_src = Regex::new(r"src=.\.\/").expect("Failed to build regex src");
+
+    let replaced_str_src = format!("src=\"{}/", gh_raw_blog_link);
+    debug!("Replaced str: {}", &replaced_str_src);
+
+    res = re_src
+        .replace_all(res.as_str(), replaced_str_src.as_str())
+        .to_string();
+    debug!("Replaced Body: {}", &res);
+
+    res
 }
