c58888346347a4dd318f81df4810b812ca38122e
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-08-18 13:51:35 +0700

    feat: change actix-web -> axum handlers

--- internal/src/handler.rs
+++ internal/src/handler.rs
@@ -1,43 +1,49 @@
 use crate::config::Config;
 use crate::model::data::BlogsData;
 use crate::router::*;
-use actix_web::{middleware, web, App, HttpServer};
+use axum::{
+    routing::{get, get_service},
+    Router,
+};
 use log::info;
+use tower_http::services::{ServeDir, ServeFile};
 
-pub async fn handler(cfg: Config) -> std::io::Result<()> {
+pub async fn handler(cfg: Config) -> () {
+    // initialize tracing and logger
+    // tracing_subscriber::fmt::init();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or(cfg.log_level.clone()));
 
     let endpoint = cfg.svc_endpoint.as_str();
-    let port = cfg
-        .svc_port
-        .parse::<u16>()
-        .expect("Failed to get port number");
+    let port = cfg.svc_port.as_str();
+    let running_endpoint = format!("{}:{}", endpoint, port);
 
     let config = cfg.clone();
+
+    // Build BlogsData
     let mut blogs_data = BlogsData::default();
     if !config.gh_owner.is_empty() && !config.gh_repo.is_empty() && !config.gh_branch.is_empty() {
         blogs_data = BlogsData::with_gh(&config.gh_owner, &config.gh_repo, &config.gh_branch).await;
     }
 
-    info!(
-        "Starting HTTP Server at http://{}:{}",
-        cfg.svc_endpoint, cfg.svc_port
-    );
+    info!("Starting HTTP Server at http://{}", running_endpoint);
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .app_data(web::Data::new(blogs_data.clone()))
-            .app_data(web::Data::new(config.clone()))
-            .service(web::resource("/").route(web::get().to(profile)))
-            .service(web::resource("/statics/{static_file}").route(web::get().to(statics)))
-            .service(web::resource("/blogs").route(web::get().to(get_blogs)))
-            .service(web::resource("/blogs/{blogid}").route(web::get().to(get_blog)))
-            .service(web::resource("/version").route(web::get().to(get_version)))
-            .service(web::resource("/not-found").route(web::get().to(get_404_not_found)))
-    })
-    .bind((endpoint, port))
-    .expect("Failed to start Http Server")
-    .run()
-    .await
+    // Axum Application
+    let app = Router::new()
+        .route("/", get(get_profile))
+        .route("/not-found", get(get_404_not_found))
+        .nest_service("/statics", get_service(ServeDir::new("./statics/favicon/")))
+        .nest_service(
+            "/statics/styles.css",
+            get_service(ServeFile::new("./statics/styles.css")),
+        )
+        .fallback(get(get_404_not_found));
+    // .route("/blogs", get(get_blogs))
+    // .route("/blogs/:blog_id", get(get_blog))
+    // .route("/version", get(get_version))
+
+    // Start Axum Application
+    let listener = tokio::net::TcpListener::bind(running_endpoint)
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
