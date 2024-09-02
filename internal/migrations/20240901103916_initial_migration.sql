3c34a7bfe72d69fd9f84b0f456aeac3536852666
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-09-03 03:35:38 +0700

    feat: add inital sqlite migration

--- internal/migrations/20240901103916_initial_migration.sql
+++ internal/migrations/20240901103916_initial_migration.sql
@@ -1,17 +1,8 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS blogs (
-    id          INTEGER PRIMARY KEY NOT NULL,
+    id          TEXT    PRIMARY KEY NOT NULL,
     name        TEXT                NOT NULL,
     source      TEXT                NOT NULL,
     filename    TEXT                NOT NULL,
     body        TEXT                NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS github_trees (
-    id          INTEGER PRIMARY KEY NOT NULL,
-    tree_path   TEXT                NOT NULL,
-    tree_mode   TEXT                NOT NULL,
-    tree_type   TEXT                NOT NULL,
-    sha         TEXT                NOT NULL,
-    url         TEXT                NOT NULL
 );
