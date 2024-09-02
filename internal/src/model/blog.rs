4c2f6c335b873b0cfc9424c89a8ce718392b1e25
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-09-03 01:34:39 +0700

    feat: implement FromRow for Blog and add BlogPagination for query param

--- internal/src/model/blog.rs
+++ internal/src/model/blog.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{sqlite::SqliteRow, Decode, FromRow};
 use std::fmt::Display;
 
 /// BlogId
@@ -114,6 +115,29 @@ pub struct Blog {
     pub body: BlogBody,
 }
 
+/// I don't knwo how it work
+/// Ref: https://stackoverflow.com/questions/78615649/how-do-i-load-sqlx-rows-to-a-struct-with-a-vector-of-structs
+impl<'r> FromRow<'r, SqliteRow> for Blog {
+    fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
+        use sqlx::Row;
+        let id = row.try_get("id")?;
+        let name = row.try_get("name")?;
+        let source = match row.try_get("source")? {
+            "github" => BlogSource::Github,
+            "filesystem" => BlogSource::FileSystem,
+        };
+        let filename = row.try_get("filename")?;
+        let body = row.try_get("body")?;
+        Ok(Blog {
+            id: BlogId(id),
+            name: BlogName(name),
+            source,
+            filename: BlogFilename(filename),
+            body: BlogBody(body),
+        })
+    }
+}
+
 /// BlogStartPage
 /// Start page of Blog Pagination
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -123,3 +147,11 @@ pub struct BlogStartPage(pub i32);
 /// End page of Blog Pagination
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct BlogEndPage(pub i32);
+
+/// BlogPagination
+/// Axum Query struct for Blog Pagination
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct BlogPagination {
+    pub start: Option<BlogStartPage>,
+    pub end: Option<BlogEndPage>,
+}
