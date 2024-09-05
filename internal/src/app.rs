8f1d70cf5f89847271bfac22a80baaa8903d4e80
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-09-06 03:08:33 +0700

    feat: update state_factory to implement new api usecases

--- internal/src/app.rs
+++ internal/src/app.rs
@@ -1,7 +1,12 @@
+use crate::api::filesystem::FilesystemApiUseCase;
+use crate::api::github::GithubApiUseCase;
 use crate::database::memory::MemoryBlogRepo;
 use crate::database::sqlite::SqliteBlogRepo;
 use crate::handler;
 use crate::model::axum::AppState;
+use crate::port::blog::command::BlogCommandPort;
+use crate::port::blog::query::BlogQueryPort;
+use crate::repo::api::ApiRepo;
 use crate::{config::Config, usecase::blog::BlogUseCase};
 use axum::{
     routing::{get, get_service},
@@ -48,25 +53,66 @@ pub async fn app() -> () {
 /// Build App State for Axum Application
 async fn state_factory(config: Config) -> AppState {
     // Setup blog use case
-    let blog_usecase = if config.data_source == "sqlite" && config.database_url != "" {
+    let data_source_is_configured_sqlite =
+        config.data_source == "sqlite" && config.database_url != "";
+    let github_api_is_enabled =
+        !config.gh_owner.is_empty() && !config.gh_repo.is_empty() && !config.gh_branch.is_empty();
+
+    let mut blog_uc = if data_source_is_configured_sqlite {
         // Use SqliteBlogRepo
         let repo = SqliteBlogRepo::new(config.database_url.clone()).await;
-        Arc::new(Mutex::new(BlogUseCase::new(Box::new(repo))))
+        BlogUseCase::new(Box::new(repo))
     } else {
-        // Use MemoryBlogRepo
-        if !config.gh_owner.is_empty() && !config.gh_repo.is_empty() && !config.gh_branch.is_empty()
-        {
-            // Use from_github method
-            let repo =
-                MemoryBlogRepo::from_github(&config.gh_owner, &config.gh_repo, &config.gh_branch)
-                    .await;
-            Arc::new(Mutex::new(BlogUseCase::new(Box::new(repo))))
-        } else {
-            // Use Default method
-            let repo = MemoryBlogRepo::default();
-            Arc::new(Mutex::new(BlogUseCase::new(Box::new(repo))))
-        }
+        // // Use MemoryBlogRepo
+        let repo = MemoryBlogRepo::default();
+        BlogUseCase::new(Box::new(repo))
+        // }
     };
+
+    let fs_usecase = FilesystemApiUseCase::new("./statics/blogs/".to_string()).await;
+    let blogs_metadata = fs_usecase.list_metadata().await;
+    for metadata in blogs_metadata {
+        // Check if blog id is in the database
+        let blog_is_not_stored = !blog_uc.check_id(metadata.id.clone()).await.0;
+        if blog_is_not_stored {
+            info!("Start to fetch Blog {}.", &metadata.id);
+            let blog = fs_usecase.fetch(metadata.clone()).await;
+            info!("Finished to fetch Blog {}.", &metadata.id);
+
+            info!("Start to store Blog {}.", &metadata.id);
+            let _ = blog_uc
+                .add(blog.id, blog.name, blog.filename, blog.source, blog.body)
+                .await;
+            info!("Finished to store Blog {}.", &metadata.id);
+        }
+    }
+
+    if github_api_is_enabled {
+        let github_usecase = GithubApiUseCase::new(
+            config.gh_owner.clone(),
+            config.gh_repo.clone(),
+            config.gh_branch.clone(),
+        )
+        .await;
+        let blogs_metadata = github_usecase.list_metadata().await;
+        for metadata in blogs_metadata {
+            // Check if blog id is in the database
+            let blog_is_not_stored = !blog_uc.check_id(metadata.id.clone()).await.0;
+            if blog_is_not_stored {
+                info!("Start to fetch Blog {}.", &metadata.id);
+                let blog = github_usecase.fetch(metadata.clone()).await;
+                info!("Finished to fetch Blog {}.", &metadata.id);
+
+                info!("Start to store Blog {}.", &metadata.id);
+                let _ = blog_uc
+                    .add(blog.id, blog.name, blog.filename, blog.source, blog.body)
+                    .await;
+                info!("Finished to store Blog {}.", &metadata.id);
+            }
+        }
+    }
+
+    let blog_usecase = Arc::new(Mutex::new(blog_uc));
 
     AppState {
         config,
