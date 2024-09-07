2a567e9764d48f3eaae0a7d1eff230212cd7b1cf
Husni Zuhdi <husni.zuhdi@accelbyte.net>
2024-09-07 16:34:58 +0700

    feat: implement Debug for dyn repo

--- internal/src/usecase/api.rs
+++ internal/src/usecase/api.rs
@@ -2,10 +2,17 @@ use crate::model::blog::{Blog, BlogMetadata};
 use crate::port::api::query::ApiQueryPort;
 use crate::repo::api::ApiRepo;
 use async_trait::async_trait;
+use core::fmt::Debug;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct ApiUseCase {
     pub api_repo: Box<dyn ApiRepo + Send + Sync>,
+}
+
+impl Debug for dyn ApiRepo + Send + Sync {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ApiRepo{:?}", self)
+    }
 }
 
 #[async_trait]
