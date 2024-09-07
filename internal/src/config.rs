2f75622beba42e6261ce18ba65dc8ccbf63f8c37
Husni Zuhdi <husni.zuhdi@accelbyte.net>
2024-09-07 16:36:56 +0700

    feat: tidy up envars setup and removal. update traing log Level

--- internal/src/config.rs
+++ internal/src/config.rs
@@ -1,12 +1,12 @@
-use log::warn;
 use std::env;
+use tracing::warn;
 
 /// Struct Config for setup environment variables
 #[derive(PartialEq, Debug, Clone)]
 pub struct Config {
     pub svc_endpoint: String,
     pub svc_port: String,
-    pub log_level: String,
+    pub log_level: tracing::Level,
     pub environment: String,
     pub data_source: String,
     pub database_url: String,
@@ -21,7 +21,7 @@ impl Default for Config {
     fn default() -> Self {
         let svc_endpoint: String = "localhost".to_string();
         let svc_port: String = "8080".to_string();
-        let log_level: String = "info".to_string();
+        let log_level = tracing::Level::INFO;
         let environment: String = "release".to_string();
         let data_source: String = "memory".to_string();
         let database_url: String = "".to_owned();
@@ -70,7 +70,22 @@ impl Config {
             .expect("failed to load SVC_PORT environment variable. Double check your config");
 
         // Optional
-        let log_level: String = Self::parse_optional_envar("LOG_LEVEL", "info");
+        let log_level: tracing::Level = match env::var("LOG_LEVEL") {
+            Err(e) => {
+                warn!(
+                    "Failed to load LOG_LEVEL environment variable. Set default to 'info'. Error {}", e
+                );
+                tracing::Level::INFO
+            }
+            Ok(val) => match val.as_str() {
+                "error" => tracing::Level::ERROR,
+                "warn" => tracing::Level::WARN,
+                "info" => tracing::Level::INFO,
+                "debug" => tracing::Level::DEBUG,
+                "trace" => tracing::Level::TRACE,
+                _ => tracing::Level::INFO,
+            },
+        };
         let environment: String = Self::parse_optional_envar("ENVIRONMENT", "release");
         let data_source: String = Self::parse_optional_envar("DATA_SOURCE", "memory");
         let database_url: String = Self::parse_optional_envar("DATABASE_URL", "");
@@ -100,7 +115,7 @@ mod test {
     fn test_default() {
         let svc_endpoint: String = "localhost".to_string();
         let svc_port: String = "8080".to_string();
-        let log_level: String = "info".to_string();
+        let log_level = tracing::Level::INFO;
         let environment: String = "release".to_string();
         let data_source: String = "memory".to_string();
         let database_url: String = "".to_string();
@@ -126,7 +141,7 @@ mod test {
         let svc_endpoint = "localhost";
         let svc_port = "8080";
         let log_level = "";
-        let expected_log_level = "info";
+        let expected_log_level = tracing::Level::INFO;
         let environment = "";
         let expected_environment = "release";
         let data_source = "";
@@ -136,15 +151,17 @@ mod test {
         let gh_repo = "";
         let gh_branch = "";
 
-        env::set_var("SVC_ENDPOINT", svc_endpoint);
-        env::set_var("SVC_PORT", svc_port);
-        env::set_var("LOG_LEVEL", log_level);
-        env::set_var("ENVIRONMENT", environment);
-        env::set_var("DATA_SOURCE", data_source);
-        env::set_var("DATABASE_URL", database_url);
-        env::set_var("GITHUB_OWNER", gh_owner);
-        env::set_var("GITHUB_REPO", gh_repo);
-        env::set_var("GITHUB_BRANCH", gh_branch);
+        set_envars(
+            svc_endpoint,
+            svc_port,
+            log_level,
+            environment,
+            data_source,
+            database_url,
+            gh_owner,
+            gh_repo,
+            gh_branch,
+        );
 
         let result = Config::from_envar();
 
@@ -158,15 +175,7 @@ mod test {
         assert_eq!(result.gh_repo, gh_repo);
         assert_eq!(result.gh_branch, gh_branch);
 
-        env::remove_var("SVC_ENDPOINT");
-        env::remove_var("SVC_PORT");
-        env::remove_var("LOG_LEVEL");
-        env::remove_var("ENVIRONMENT");
-        env::remove_var("DATA_SOURCE");
-        env::remove_var("DATABASE_URL");
-        env::remove_var("GITHUB_OWNER");
-        env::remove_var("GITHUB_REPO");
-        env::remove_var("GITHUB_BRANCH");
+        remove_envars()
     }
 
     #[test]
@@ -174,6 +183,7 @@ mod test {
         let svc_endpoint = "localhost";
         let svc_port = "8080";
         let log_level = "info";
+        let expected_log_level = tracing::Level::INFO;
         let environment = "dev";
         let data_source = "sqlite";
         let database_url = "sqlite:husni-portfolio.db";
@@ -181,6 +191,44 @@ mod test {
         let gh_repo = "husni-blog-resources";
         let gh_branch = "main";
 
+        set_envars(
+            svc_endpoint,
+            svc_port,
+            log_level,
+            environment,
+            data_source,
+            database_url,
+            gh_owner,
+            gh_repo,
+            gh_branch,
+        );
+
+        let result = Config::from_envar();
+
+        assert_eq!(result.svc_endpoint, svc_endpoint);
+        assert_eq!(result.svc_port, svc_port);
+        assert_eq!(result.log_level, expected_log_level);
+        assert_eq!(result.environment, environment);
+        assert_eq!(result.data_source, data_source);
+        assert_eq!(result.database_url, database_url);
+        assert_eq!(result.gh_owner, gh_owner);
+        assert_eq!(result.gh_repo, gh_repo);
+        assert_eq!(result.gh_branch, gh_branch);
+
+        remove_envars()
+    }
+
+    fn set_envars(
+        svc_endpoint: &str,
+        svc_port: &str,
+        log_level: &str,
+        environment: &str,
+        data_source: &str,
+        database_url: &str,
+        gh_owner: &str,
+        gh_repo: &str,
+        gh_branch: &str,
+    ) {
         env::set_var("SVC_ENDPOINT", svc_endpoint);
         env::set_var("SVC_PORT", svc_port);
         env::set_var("LOG_LEVEL", log_level);
@@ -190,19 +238,9 @@ mod test {
         env::set_var("GITHUB_OWNER", gh_owner);
         env::set_var("GITHUB_REPO", gh_repo);
         env::set_var("GITHUB_BRANCH", gh_branch);
+    }
 
-        let result = Config::from_envar();
-
-        assert_eq!(result.svc_endpoint, svc_endpoint);
-        assert_eq!(result.svc_port, svc_port);
-        assert_eq!(result.log_level, log_level);
-        assert_eq!(result.environment, environment);
-        assert_eq!(result.data_source, data_source);
-        assert_eq!(result.database_url, database_url);
-        assert_eq!(result.gh_owner, gh_owner);
-        assert_eq!(result.gh_repo, gh_repo);
-        assert_eq!(result.gh_branch, gh_branch);
-
+    fn remove_envars() {
         env::remove_var("SVC_ENDPOINT");
         env::remove_var("SVC_PORT");
         env::remove_var("LOG_LEVEL");
