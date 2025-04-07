fecf38037924fa26b93a6570ba35fa4740b2d94a
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2025-04-07 18:42:44 +0700

    feat: update fe implementation and fix some edge cases related to missing/unavailable paths

--- internal/src/handler/blogs.rs
+++ internal/src/handler/blogs.rs
@@ -2,13 +2,13 @@ use crate::handler::status::{get_404_not_found, get_500_internal_server_error};
 use crate::model::blogs::{BlogEndPage, BlogId, BlogPagination, BlogStartPage};
 use crate::model::{
     axum::AppState,
-    templates::{BlogTemplate, BlogsTemplate, BlogsTemplateBlog},
+    templates::{BlogMetadataTemplate, BlogTemplate, BlogsTemplate},
 };
 use askama::Template;
 use axum::debug_handler;
 use axum::extract::{Path, Query, State};
 use axum::response::Html;
-use tracing::{debug, error, info};
+use tracing::{debug, error, info, warn};
 
 /// get_blogs
 /// Serve get_blogs HTML file
@@ -42,12 +42,12 @@ pub async fn get_blogs(
     let result = data.blog_repo.find_blogs(start.clone(), end.clone()).await;
     match result {
         Some(blogs_data) => {
-            let blogs: Vec<BlogsTemplateBlog> = blogs_data
+            let blogs: Vec<BlogMetadataTemplate> = blogs_data
                 .iter()
                 .map(|blog| {
                     debug!("Construct BlogsTemplateBlog for Blog Id {}", &blog.id);
-                    BlogsTemplateBlog {
-                        id: &blog.id.as_str(),
+                    BlogMetadataTemplate {
+                        id: &blog.id.id,
                         name: &blog.name.as_str(),
                     }
                 })
@@ -81,15 +81,34 @@ pub async fn get_blogs(
 /// Render our blog
 #[debug_handler]
 pub async fn get_blog(Path(path): Path<String>, State(app_state): State<AppState>) -> Html<String> {
+    // Sanitize `path`
+    let id = path.parse::<i64>();
+    match &id {
+        Ok(val) => {
+            debug!("Successfully parse path {} into {} i64", &path, &val);
+        }
+        Err(err) => {
+            warn!("Failed to parse path {} to i64. Err: {}", &path, err);
+            // TIL i forgot this kind of early return exist in rust.
+            // TBH I forgot to use `return` keyword lmao
+            return get_404_not_found().await;
+        }
+    }
+
     // Locking Mutex
     let data = app_state.blog_usecase.lock().await;
 
     // Construct BlogTemplate Struct
-    let result = data.blog_repo.find(BlogId { id: path.clone() }).await;
+    let result = data
+        .blog_repo
+        .find(BlogId {
+            id: id.clone().unwrap(),
+        })
+        .await;
     match result {
         Some(blog_data) => {
             let blog = BlogTemplate {
-                id: path.clone().as_str(),
+                id: &id.clone().unwrap(),
                 name: &blog_data.name.as_str(),
                 filename: &blog_data.filename.as_str(),
                 body: &blog_data.body.as_str(),
@@ -108,7 +127,7 @@ pub async fn get_blog(Path(path): Path<String>, State(app_state): State<AppState
             }
         }
         None => {
-            error!("Failed to find Blog with Id {}.", &path);
+            info!("Failed to find Blog with Id {}.", &path);
             get_404_not_found().await
         }
     }
