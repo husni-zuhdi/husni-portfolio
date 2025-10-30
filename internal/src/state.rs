8ec0140848dc0d289dfec703783873f2dec0c26d
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2025-10-30 16:05:21 +0700

    chore: remove github and filesystem from state factory and initiate unit test

--- internal/src/state.rs
+++ internal/src/state.rs
@@ -1,20 +1,33 @@
-use crate::api::github::GithubApiUseCase;
 use crate::config::Config;
 use crate::database::memory::MemoryBlogRepo;
 use crate::database::turso::TursoDatabase;
 use crate::model::axum::AppState;
-use crate::repo::api::ApiRepo;
-use crate::repo::blogs::BlogRepo;
 use crate::usecase::auth::AuthUseCase;
 use crate::usecase::blog_tag_mappings::BlogTagMappingUseCase;
 use crate::usecase::blogs::BlogUseCase;
 use crate::usecase::tags::TagUseCase;
-use crate::{api::filesystem::FilesystemApiUseCase, usecase::talks::TalkUseCase};
+use crate::usecase::talks::TalkUseCase;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use tracing::{debug, info};
+use tracing::info;
 
 /// Build App State for Axum Application
+/// take a Config and return AppState
+/// AppState contains `Config` and `BlogUseCase`
+/// and can contain optional:
+///
+/// - TalkUseCase
+/// - TagUseCase
+/// - BlogTagMappingUseCase
+/// - AuthUseCase
+///
+/// To have a fully function portfolio web-app, it's sugessted to enable
+/// all usecases.
+///
+/// Deprecated features:
+///
+/// - Filesystem blogs detection
+/// - Populate blogs from github repository
 pub async fn state_factory(config: Config) -> AppState {
     // Setup blog use case
     let data_source_is_configured_sqlite =
@@ -22,11 +35,10 @@ pub async fn state_factory(config: Config) -> AppState {
     let data_source_is_configured_turso = config.data_source == "turso"
         && config.secrets.database_url.is_some()
         && config.secrets.turso_auth_token.is_some();
-    let github_api_is_enabled =
-        config.gh_owner.is_some() && config.gh_repo.is_some() && config.gh_branch.is_some();
 
-    let (mut blog_uc, talk_uc, tag_uc, blog_tag_mapping_uc, auth_uc) =
+    let (blog_uc, talk_uc, tag_uc, blog_tag_mapping_uc, auth_uc) =
         if data_source_is_configured_sqlite {
+            info!("Building SQLite usecases.");
             let repo = TursoDatabase::new(
                 config.data_source.clone(),
                 config.secrets.database_url.clone().unwrap(),
@@ -41,6 +53,7 @@ pub async fn state_factory(config: Config) -> AppState {
                 Some(AuthUseCase::new(Box::new(repo))),
             )
         } else if data_source_is_configured_turso {
+            info!("Building Turso usecases.");
             let repo = TursoDatabase::new(
                 config.data_source.clone(),
                 config.secrets.database_url.clone().unwrap(),
@@ -59,21 +72,6 @@ pub async fn state_factory(config: Config) -> AppState {
             (BlogUseCase::new(Box::new(repo)), None, None, None, None)
         };
 
-    if config.filesystem_dir.is_some() {
-        let fs_usecase = FilesystemApiUseCase::new(config.filesystem_dir.clone().unwrap()).await;
-        let _ = populate_blog(Box::new(fs_usecase), &mut blog_uc).await;
-    }
-
-    if github_api_is_enabled {
-        let github_usecase = GithubApiUseCase::new(
-            config.gh_owner.clone().unwrap(),
-            config.gh_repo.clone().unwrap(),
-            config.gh_branch.clone().unwrap(),
-        )
-        .await;
-        let _ = populate_blog(Box::new(github_usecase), &mut blog_uc).await;
-    }
-
     let blog_usecase = Arc::new(Mutex::new(blog_uc));
     let talk_usecase = Arc::new(Mutex::new(talk_uc));
     let tag_usecase = Arc::new(Mutex::new(tag_uc));
@@ -90,19 +88,49 @@ pub async fn state_factory(config: Config) -> AppState {
     }
 }
 
-async fn populate_blog(api_uc: Box<dyn ApiRepo + Send + Sync>, blog_uc: &mut BlogUseCase) {
-    let blogs_metadata = api_uc.list_metadata().await.unwrap();
-    for metadata in blogs_metadata {
-        let blog_is_not_stored = blog_uc.check_id(metadata.id).await.is_none();
-        if blog_is_not_stored {
-            info!("Start to populate Blog {}.", &metadata.id);
-            debug!("Start to fetch Blog {}.", &metadata.id);
-            let blog = api_uc.fetch(metadata.clone()).await.unwrap();
-            debug!("Finished to fetch Blog {}.", &metadata.id);
+#[cfg(test)]
+mod test {
+    use super::*;
 
-            debug!("Start to store Blog {}.", &metadata.id);
-            let _ = blog_uc.add(blog).await;
-            debug!("Finished to store Blog {}.", &metadata.id);
-        }
+    #[tokio::test]
+    async fn test_memory_state_factory() {
+        // Config default data source is `memory`
+        let config = Config::default();
+        let state = state_factory(config).await;
+        // Assume `BlogUseCase` always created.
+        let talk_uc = state.talk_usecase.lock().await.take();
+        let tag_uc = state.tag_usecase.lock().await.take();
+        let blogtag_uc = state.blog_tag_mapping_usecase.lock().await.take();
+        let auth_uc = state.auth_usecase.lock().await.take();
+
+        assert!(talk_uc.is_none(), "TalkUseCase is Some()");
+        assert!(tag_uc.is_none(), "TagUseCase is Some()");
+        assert!(blogtag_uc.is_none(), "BlogTagMappingUseCase is Some()");
+        assert!(auth_uc.is_none(), "AuthUseCase is Some()");
     }
+
+    #[tokio::test]
+    async fn test_sqlite_state() {
+        // Config default data source is `memory`
+        let mut config = Config::default();
+        config.data_source = "sqlite".to_string();
+        config.secrets.database_url = Some("../husni-portfolio.db".to_string());
+
+        let state = state_factory(config).await;
+        // Assume `BlogUseCase` always created.
+        let talk_uc = state.talk_usecase.lock().await.take();
+        let tag_uc = state.tag_usecase.lock().await.take();
+        let blogtag_uc = state.blog_tag_mapping_usecase.lock().await.take();
+        let auth_uc = state.auth_usecase.lock().await.take();
+
+        assert!(talk_uc.is_some(), "TalkUseCase is None");
+        assert!(tag_uc.is_some(), "TagUseCase is None");
+        assert!(blogtag_uc.is_some(), "BlogTagMappingUseCase is None");
+        assert!(auth_uc.is_some(), "AuthUseCase is None");
+    }
+
+    //#[tokio::test]
+    //async fn test_turso_state() {
+    //    todo!()
+    //}
 }
