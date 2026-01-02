064a7dca6642a189bb87053d618b8a7cd4c5bf2f
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2026-01-02 23:20:57 +0700

    chore: add talks inmemory unit tests

--- src/cache/inmemory/talks.rs
+++ src/cache/inmemory/talks.rs
@@ -39,6 +39,9 @@ impl TalkDisplayRepo for InMemoryCache {
 
 #[async_trait]
 impl TalkOperationRepo for InMemoryCache {
+    /// Insert Talk Cache
+    /// Take a `Talk` object and store it in the `InMemoryCache`
+    /// Return Option of `TalkCommandStatus`. If `None`, insertion failed
     async fn insert(&mut self, talk: Talk) -> Option<TalkCommandStatus> {
         info!("Inserting Talk with id {} into InMemoryCache", &talk.id);
         let key = format!("talk-{}", &talk.id);
@@ -50,5 +53,128 @@ impl TalkOperationRepo for InMemoryCache {
         let key = format!("talk-{id}");
         self.talks_cache.invalidate(&key).await;
         Some(TalkCommandStatus::CacheInvalidated)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_insert_find_and_invalidate_talk() {
+        let cache = InMemoryCache::new(3600).await;
+        let test_val = Talk {
+            id: 1,
+            name: "test".to_string(),
+            date: "2025-10-10".to_string(),
+            media_link: None,
+            org_name: None,
+            org_link: None,
+        };
+
+        // Insert cache
+        let insert_status = cache.clone().insert(test_val.clone()).await;
+        assert!(insert_status.is_some(), "talk-1 insertion failed");
+        assert_eq!(insert_status.unwrap(), TalkCommandStatus::CacheInserted);
+
+        // Find cache
+        let result = cache.clone().find(test_val.id).await;
+        assert!(result.is_some(), "talk-1 find operation failed");
+        assert_eq!(result.unwrap(), test_val.clone());
+
+        // Invalidate cache
+        let invalidate_status = cache.clone().invalidate(test_val.id).await;
+        assert!(invalidate_status.is_some(), "talk-1 invalidation failed");
+        assert_eq!(
+            invalidate_status.unwrap(),
+            TalkCommandStatus::CacheInvalidated
+        );
+
+        // Find cache after invalidation
+        let result = cache.clone().find(test_val.id).await;
+        assert!(result.is_none(), "talk-1 find a cache! it shouldn't be!");
+    }
+
+    #[tokio::test]
+    async fn test_insert_find_and_invalidate_talks() {
+        let cache = InMemoryCache::new(3600).await;
+        let test_values = Talks {
+            // Intentionally reversed order
+            talks: vec![
+                Talk {
+                    id: 2,
+                    name: "tast".to_string(),
+                    date: "2025-10-10".to_string(),
+                    media_link: Some("https://youtube.com".to_string()),
+                    org_name: None,
+                    org_link: None,
+                },
+                Talk {
+                    id: 1,
+                    name: "test".to_string(),
+                    date: "2025-10-10".to_string(),
+                    media_link: None,
+                    org_name: None,
+                    org_link: None,
+                },
+            ],
+        };
+
+        for test_val in &test_values.talks {
+            let id = test_val.id;
+            // Insert cache
+            let insert_status = cache.clone().insert(test_val.clone()).await;
+            assert!(
+                insert_status.is_some(),
+                "{}",
+                format!("Talk-{id} insertion failed")
+            );
+            assert_eq!(insert_status.unwrap(), TalkCommandStatus::CacheInserted);
+
+            // Find cache
+            let result = cache.clone().find(id).await;
+            assert!(
+                result.is_some(),
+                "{}",
+                format!("talk-{id} find operation failed")
+            );
+            assert_eq!(result.unwrap(), test_val.clone());
+        }
+
+        // Find talk caches
+        let talks_res = cache
+            .clone()
+            .find_talks(TalksParams {
+                start: Some(0),
+                end: Some(2),
+            })
+            .await;
+        assert!(talks_res.is_some(), "find_talks operation failed");
+        assert_eq!(talks_res.unwrap(), test_values.clone());
+
+        for test_val in &test_values.talks {
+            let id = test_val.id;
+            // Invalidate cache
+            let invalidate_status = cache.clone().invalidate(id).await;
+            assert!(
+                invalidate_status.is_some(),
+                "{}",
+                format!("talk-{id} invalidation failed")
+            );
+            assert_eq!(
+                invalidate_status.unwrap(),
+                TalkCommandStatus::CacheInvalidated
+            );
+        }
+
+        // Find talk caches after invalidated
+        let talks_res = cache
+            .clone()
+            .find_talks(TalksParams {
+                start: Some(0),
+                end: Some(2),
+            })
+            .await;
+        assert!(talks_res.is_none(), "find_talks operation should failed!");
     }
 }
